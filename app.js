13a1f400c699ace1f171ddbb21560f92c28df0f0
michaellee212 <33797607+michaellee212@users.noreply.github.com>
2019-08-09 15:36:00 -0700

    final
    fina;

---
+++ app.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+// var app = express();
+// const port = 3000;
+
+// app.set("view engine", "ejs");
+
+
+// app.get("/", (req, res) => {
+//     res.render("index.ejs");
+// });
+
+
+// // Express listens for requests (Start server)
+// app.listen(port, () => console.log(`Portfolio starting on port ${port}!`))
